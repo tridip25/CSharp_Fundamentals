d9e808babdde82724f7d98c1c4521cfbf7df9067
tridip25 <74949104+tridip25@users.noreply.github.com>
2020-12-03 20:58:11 +0530

    Add files via upload

---
+++ GradeBook/test/GradeBook.Tests/TypeTests.cs
@@ -0,0 +1,193 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+
+    //declaring a delegate
+    public delegate string WriteLogDelegate(string logMessage);
+    public class TypeTests
+    {
+          
+    //delegate..................................
+    int count = 0;
+
+   [Fact]
+    public void WriteLogDelegateCanPointToMethod()
+    {
+        WriteLogDelegate log = ReturnMessage;
+
+        log += ReturnMessage;
+        log += IncrementCount;
+
+        var result = log("Hello!");
+        Assert.Equal(3, count);
+    }
+
+    string IncrementCount(string message)
+    {
+        count++;
+        return message.ToLower();
+    }
+   string ReturnMessage(string message)
+    {
+        count++;
+        return message;
+    }
+   
+   
+   
+   
+   /* [Fact]
+    public void WriteLogDelegateCanPointToMethod()
+    {
+        WriteLogDelegate log;
+
+        log = ReturnMessage;
+
+        var result = log("Hello!");
+        Assert.Equal("Hello!", result);
+    }
+
+    string ReturnMessage(string message)
+    {
+        return message;
+    }
+
+
+*/
+
+
+
+
+
+
+     //Value types.............................
+
+     [Fact]
+    public void ValueTypeAlsoPassByRef()
+    {
+        var x = 3;
+        SetInt(ref x);
+        Assert.Equal(42,x);
+    }
+
+        private void SetInt(ref int z)
+        {
+           z=42;
+        }
+
+      
+
+
+
+
+
+    [Fact]
+    public void ValueTypeAlsoPassByValue()
+    {
+        var x = GetInt();
+        SetInt(x);
+        Assert.Equal(3,x);
+    }
+
+        private void SetInt(int z)
+        {
+           z=42;
+        }
+
+        private int GetInt()
+        {
+           return 3;
+        }
+
+
+
+
+
+
+
+
+        //  Below given are references types...(i.e defined by class).......................................
+        [Fact]
+        public void CsharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+    
+        }
+
+        private void GetBookSetName(ref InMemoryBook book, string name)
+        {
+            book = new InMemoryBook(name);
+          //  book.Name = name;   //just for fun......
+        }
+
+        [Fact]
+        public void CsharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+    
+        }
+
+        private void GetBookSetName(InMemoryBook book, string name)
+        {
+            book = new InMemoryBook(name);
+            book.Name = name;   //just for fun......
+        }
+
+        //In C# whenever you pass a parameter to a method it's
+        //always pass by value(unless you use a ref keyword) 
+        //..same in the case below but tricky try to understand... 
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            //sending the value inside book1 and passing a reference of it ..not the reference of the object.................
+            SetName(book1, "New Name"); 
+            
+
+            Assert.Equal("New Name", book1.Name);
+    
+        }
+
+        private void SetName(InMemoryBook book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
+
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 2", book2.Name);
+        }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+           // Assert.Equal("Book 1", book1.Name);
+           // Assert.Equal("Book 1", book2.Name);
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1,book2));//tells whether variables in these variables pointing to the same object
+        }
+
+
+        InMemoryBook GetBook(string name)
+        {
+
+            return new InMemoryBook(name);
+        }
+    }
+}
