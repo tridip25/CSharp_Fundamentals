df17114d7d21680884dc5df24309aabea981e47e
tridip25 <74949104+tridip25@users.noreply.github.com>
2020-12-03 20:41:58 +0530

    Add files via upload

---
+++ sum_of_no/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace sum_of_no
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 5.6;  // also you can use var keyword, which will automatically choose the data type
+                            // var x=3.5; (x is float) , var x=3; (x is integer now)
+            double y = 9.8;
+            double result = x + y;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ sum_of_no_using_array/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace sum_of_no_using_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new []{12.7, 10.3, 6.11, 4.1};
+            var result = 0.0;
+            foreach(var number in numbers)
+            {
+                result+= number;
+            }
+            Console.WriteLine("Sum of the numbers in the array is: "+result);
+        }
+    }
+}
